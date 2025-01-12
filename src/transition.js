79d435c546989f8d4ea4854c6e7ac77f0e87bd12
teobaso <teobasol4@gmail.com>
2025-01-12 12:21:59 +0100

    page transition

---
+++ src/transition.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loader = document.getElementById("loader");
+    const content = document.getElementById("content");
+    const progressBar = document.querySelector(".progress-bar .progress");
+
+    // Simuler un chargement progressif
+    let progress = 0;
+    const loadingInterval = setInterval(() => {
+        progress += 10;
+        progressBar.style.width = `${progress}%`;
+
+        // Lorsque le chargement atteint 100%, on masque le loader
+        if (progress >= 100) {
+            clearInterval(loadingInterval);
+            loader.style.display = "none";
+            content.style.display = "block";
+        }
+    }, 200); // Augmenter la progression toutes les 200 ms
+});
