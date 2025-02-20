c3ee50e7f0d8337579db8f29f9aa5af75165adc8
teobaso <teobasol4@gmail.com>
2025-02-20 16:47:41 +0100

    test responsive certif

--- src/transition.js
+++ src/transition.js
@@ -15,27 +15,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }, 200); // Augmenter la progression toutes les 200 ms
 });
-
-
-function hasTouch() {
-    return 'ontouchstart' in document.documentElement
-        || navigator.maxTouchPoints > 0
-        || navigator.msMaxTouchPoints > 0;
-}
-
-if (hasTouch()) { // remove all the :hover stylesheets
-    try { // prevent exception on browsers not supporting DOM styleSheets properly
-        for (let si in document.styleSheets) {
-            let styleSheet = document.styleSheets[si];
-            if (!styleSheet.rules) continue;
-
-            for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                if (!styleSheet.rules[ri].selectorText) continue;
-
-                if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                    styleSheet.deleteRule(ri);
-                }
-            }
-        }
-    } catch (ex) {}
-}
